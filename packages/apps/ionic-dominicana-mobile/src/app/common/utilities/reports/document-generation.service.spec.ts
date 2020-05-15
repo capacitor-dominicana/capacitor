07bdef08d5978b98e8155e727f787e0e60a82967
1antares1 <koaltares@outlook.com>
2020-05-15 09:10:24 -0400

    [Formatting] Some files were indented and formatted based on parameters

--- packages/apps/ionic-dominicana-mobile/src/app/common/utilities/reports/document-generation.service.spec.ts
+++ packages/apps/ionic-dominicana-mobile/src/app/common/utilities/reports/document-generation.service.spec.ts
@@ -1,16 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { DocumentGenerationService } from './document-generation.service';
+import { DocumentGenerationService } from "./document-generation.service";
 
-describe('DocumentGenerationService', () => {
-  let service: DocumentGenerationService;
+describe("DocumentGenerationService", () => {
+    let service: DocumentGenerationService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DocumentGenerationService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(DocumentGenerationService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it("should be created", () => {
+        expect(service).toBeTruthy();
+    });
 });
